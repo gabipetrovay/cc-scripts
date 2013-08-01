81ab92abbde893e1c865ead6de45446e9dfb444c
Ionică Bizău <bizauionica@yahoo.com>
2013-08-01 11:27:02 +0300

    Create switchDetail.js

---
+++ switchDetail.js
@@ -0,0 +1,13 @@
+setTimeout(function () {
+        $('#contentContainer').on('click', '#dataTable', function() {
+            $('#leftContainer').hide();
+            $('#dataContainer').hide();
+            $('#detailContainer').show();
+        });
+
+        $('#contentContainer').on('click', '#detailBackBtn', function() {
+            $('#detailContainer').hide();
+            $('#leftContainer').show();
+            $('#dataContainer').show();
+        });
+}, 3000);
